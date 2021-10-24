5e8f0e6b5c4f4bdeccd7c319343b9a663b2f0e4a
Claudio Bernardes <bernardes.claudio@gmail.com>
2021-10-24 23:20:44 +0200

    Auth with tesla

--- lib/spotify/auth_request.ex
+++ lib/spotify/auth_request.ex
@@ -4,13 +4,20 @@ defmodule Spotify.AuthRequest do
   @url "https://accounts.spotify.com/api/token"
 
   def post(params) do
-    HTTPoison.post(@url, params, headers())
+    client = headers()
+    Tesla.post(client, @url, params)
   end
 
-  def headers do
-    [
-      {"Authorization", "Basic #{Spotify.encoded_credentials()}"},
-      {"Content-Type", "application/x-www-form-urlencoded"}
+  def headers() do
+    middleware = [
+      {Tesla.Middleware.Headers,
+       [
+        {"Authorization", "Basic #{Spotify.encoded_credentials()}"},
+        {"Content-Type", "application/x-www-form-urlencoded"}
+       ]}
     ]
+    adapter = {Tesla.Adapter.Hackney, [recv_timeout: 30_000]}
+    Tesla.client(middleware, adapter)
   end
+
 end
